4acdc36d23c6651e97806c95fa7b6590145822bd
Haseeb745 <63511481+Haseeb745@users.noreply.github.com>
2023-03-27 16:16:51 +0530

    changes to flower braces in xlsx 132 and lines

--- target26_1.js
+++ target26_1.js
@@ -129,9 +129,9 @@ looker.plugins.visualizations.add({
         // var note = [["* All values reported are in millions"]];
         // document.write("<span style='font-family:serif; text-align: left; font-weight:normal; font-size:10px; align-items:left; border:1px solid black; background-color: #eee;'>"+note+"</span>");
         // note[0].style.font = "10pt serif";
-        var wsheet = XLSX.utils.table_to_sheet(data, {origin: 'A3'});
-        XLSX.utils.sheet_add_aoa(wsheet, header, { origin: 'A1' });
-        XLSX.utils.sheet_add_aoa(wsheet, note, { origin: 'A2' });
+        var wsheet = XLSX.utils.table_to_sheet(data, [origin: 'A3']);
+        XLSX.utils.sheet_add_aoa(wsheet, header, [ origin: 'A1' ]);
+        XLSX.utils.sheet_add_aoa(wsheet, note, [ origin: 'A2' ]);
         var wbook = XLSX.utils.book_new();
         XLSX.utils.book_append_sheet(wbook, wsheet, "C26");
         var wbexport = XLSX.write(wbook, {
