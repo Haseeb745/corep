af5725da60d576c57ef19ceb62eff83e9f331a9f
Haseeb745 <63511481+Haseeb745@users.noreply.github.com>
2023-03-27 16:42:07 +0530

    Update and rename target26_3.js to target26_4.js
    changes back to first version

--- target26_4.js
+++ target26_4.js
@@ -95,10 +95,10 @@ looker.plugins.visualizations.add({
     this._container.prepend(downloadButton);
     downloadButton.addEventListener('click', () => { 
 
-      const htmlTable = document.querySelector('table');
+      var htmlTable = document.querySelector('table');
       // htmlTable.style.border = '1px solid black';
       // htmlTable.style.fontSize = '11px';
-      const rows = htmlTable.rows;
+      var rows = htmlTable.rows;
       // rows[0].innerHTML =  "<tr class='table-header'><th class='table-header' rowspan='1' colspan='"+(k+2)+"' style='align-items: left;text-align: left; height: 40px;border: 1px solid black;background-color: #eee;font-family: verdana;'><b>C 26.00 - Large Exposures limits (LE Limits)</b></th></tr>";
       // rows[1].innerHTML =  "<tr class='table-header'><th class='table-header' rowspan='1' colspan='3' style='background-color:none !important;font-family:verdana;font-size:10px;align-items: center;text-align: left;padding: 5px;color:grey;font-weight:normal;'>* All values reported are in millions </th></tr>";
       for (var i = 0; i < rows.length; i++) {
@@ -108,11 +108,11 @@ looker.plugins.visualizations.add({
           }
       }
 
-        const type = "xlsx";
+        var type = "xlsx";
         // var ctx = { Worksheet: 'C26', table: htmlTable.in };
         // var ctx = { Worksheet: 'C26', table: "<tr class='table-header'><th class='table-header' rowspan='1' colspan='100' style='align-items: left;text-align: left; height: 40px;border: 1px solid black;background-color: #eee;font-family: Verdana;'><b>C 29.00 - Detail of the exposures to individual clients within groups of connected clients (LE 3)</b></th></tr><tr class='table-header'><th class='table-header' rowspan='1' colspan='3' style='background-color:none !important;font-family:Verdana;font-size:10px;align-items: center;text-align: right;padding: 5px;color:grey;font-weight:normal;'>* All values reported are in millions </th></tr>" + htmlTable.innerHTML };
-        const data = htmlTable;
-        const header = document.createElement('span');
+        var data = htmlTable;
+        var header = document.createElement('span');
         header.style.fontWeight = "bold";
         header.style.fontFamily = "verdana";
         header.style.fontSize = "14pt";
@@ -120,7 +120,7 @@ looker.plugins.visualizations.add({
         // header = [["C 26.00 - Large Exposures limits (LE Limits)"]];
         // header[0].style.font = "bold 14pt verdana";
         // document.write("<span style='font-family:verdana; text-align: left; font-weight:bold; font-size:14px; align-items:left; border:1px solid black; background-color: #eee;'>"+header+"</span>");
-        const note = document.createElement('span');
+        var note = document.createElement('span');
         note.style.fontWeight = "bold";
         note.style.fontFamily = "Verdana", "Geneva", "sans-serif";
         note.style.fontSize = "14pt";
@@ -129,12 +129,12 @@ looker.plugins.visualizations.add({
         // var note = [["* All values reported are in millions"]];
         // document.write("<span style='font-family:serif; text-align: left; font-weight:normal; font-size:10px; align-items:left; border:1px solid black; background-color: #eee;'>"+note+"</span>");
         // note[0].style.font = "10pt serif";
-        const wsheet = XLSX.utils.table_to_sheet(data, {origin: 'A3'});
+        var wsheet = XLSX.utils.table_to_sheet(data, {origin: 'A3'});
         XLSX.utils.sheet_add_aoa(wsheet, header, { origin: 'A1' });
         XLSX.utils.sheet_add_aoa(wsheet, note, { origin: 'A2' });
         var wbook = XLSX.utils.book_new();
         XLSX.utils.book_append_sheet(wbook, wsheet, "C26");
-        const wbexport = XLSX.write(wbook, {
+        var wbexport = XLSX.write(wbook, {
             bookType: type,
             bookSST: true,
             type: 'binary',
